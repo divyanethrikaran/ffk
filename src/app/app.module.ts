3cebc4233c7d51524bb0f7c15776837ae1310833
divya.n <developer.pe@technovert.com>
2023-10-26 10:48:41 +0530

    added table for shared component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { TableComponentComponent } from './table-component/table-component.component';
+import { ModalComponentComponent } from './modal-component/modal-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    TableComponentComponent
   ],
   providers: [],
   bootstrap: [AppComponent]

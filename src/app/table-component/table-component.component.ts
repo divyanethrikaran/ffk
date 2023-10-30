efa965a55a27127d26f43654291d554c82d7169a
divya.n <developer.pe@technovert.com>
2023-10-30 12:12:13 +0530

    added server side pagination

--- src/app/table-component/table-component.component.ts
+++ src/app/table-component/table-component.component.ts
@@ -1,11 +1,18 @@
-import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AfterViewInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
+import { PeriodicElement } from '../app.component';
 
+
+interface paginationDetail {
+  currentPage: number;
+  totalPages: number;
+  totalItems: number;
+  itemsPerPage: number;
+}
 @Component({
   selector: 'app-table-component',
   templateUrl: './table-component.component.html',
@@ -14,54 +21,40 @@ import { CommonModule } from '@angular/common';
   imports: [MatTableModule, MatPaginatorModule, MatSortModule, CommonModule],
 })
 export class TableComponentComponent implements AfterViewInit {
+  @Input() columnDefinitions!: any[];
+  @Input() ELEMENT_DATA!: any[];
+  @Input() isPaginationVisible = true;
+  @Input() pageSize! : number
+  @Output() paginationResult = new EventEmitter<paginationDetail>(); 
 
-  columnDefinitions = [
-    { name: 'position', header: 'No.' },
-    { name: 'name', header: 'Name' },
-    { name: 'weight', header: 'Weight' },
-    { name: 'symbol', header: 'Symbol' },
-  ];
-  columnNames = this.columnDefinitions.map((column) => column.name);
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
-  isPaginationVisible = true;
+  columnNames: any;
+  dataSource: any;
+  paginationDetail! : paginationDetail;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor() {}
 
+  ngOnInit() {
+    this.columnNames = this.columnDefinitions?.map((column) => column.name);
+    this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+
+  }
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
-}
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  togglePaginationVisibility() {
+    this.isPaginationVisible = !this.isPaginationVisible;
+  }
+  
+  pageEvents(event : any) {
+    this.paginationDetail.currentPage= event.pageIndex;
+    this.paginationDetail.totalPages=(event.length % event.pageSize == 0) ? (event.length / event.pageSize) : (event.length / event.pageSize)+1
+    this.paginationDetail.totalItems = event.length;
+    this.paginationDetail.itemsPerPage = event.pageSize;
+    this.paginationResult.emit(this.paginationDetail)
+  }
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
-];
